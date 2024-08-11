2229bd7caf96dfc53512fa039ae597bdd26735de
Timo <timomenke@gmail.com>
2024-08-11 16:04:55 +0200

    add dactyl manuform

---
+++ include/zmk-helpers/key-labels/dactyl_manuform_4x5.h
@@ -0,0 +1,70 @@
+/*                                      46 KEY MATRIX / LAYOUT MAPPING
+
+  ╭────────────────────────────┬────────────────────────────╮
+  │  0   1   2   3   4         │          5   6   7   8   9 │
+  │ 10  11  12  13  14         │         15  16  17  18  19 │
+  │ 20  21  22  23  24         │         25  26  27  28  29 │
+  ╰───╮ 30  31  32  33  34  35 │ 36  37  38  39  40  41 ╭───╯
+      ╰──────────────╮  42  43 │ 44  45  ╭──────────────╯
+                     ╰─────────┴─────────╯             */
+
+
+#pragma once
+
+#define LT0  0  // left-top row
+#define LT1  1
+#define LT2  2
+#define LT3  3
+#define LT4  4
+
+#define RT0  5  // right-top row
+#define RT1  6
+#define RT2  7
+#define RT3  8
+#define RT4  9
+
+#define LM0 10  // left-middle row
+#define LM1 11
+#define LM2 12
+#define LM3 13
+#define LM4 14
+
+#define RM0 15  // right-middle row
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+#define RM5 20
+
+#define LB0 20  // left-bottom row
+#define LB1 21
+#define LB2 22
+#define LB3 23
+#define LB4 24
+
+#define RB0 25  // right-bottom row
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
+
+#define LTT0 30  // left-top thumb keys
+#define LTT1 31
+#define LTT2 32
+#define LTT3 33
+#define LTT4 34
+#define LTT5 35
+
+#define RTT0 36  // right-top thumb keys
+#define RTT1 37
+#define RTT2 38
+#define RTT4 39
+#define RTT5 40
+#define RTT6 41
+
+#define LBT0 42  // left-bottom thumb keys
+#define LBT1 43
+
+#define RBT0 44  // right-bottom thumb keys
+#define RBT1 45
+
